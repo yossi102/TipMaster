115ee079ab8fd68dff24c7f8aef79f160df4f5c7
yossi102 <yossi0102@gmail.com>
2020-05-23 14:51:10 +0300

    Set IBOutlets and IBAction

--- TipMaster/ViewController.swift
+++ TipMaster/ViewController.swift
@@ -10,11 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var billTextField: UITextField!
+    @IBOutlet weak var zeroPctButton: UIButton!
+    @IBOutlet weak var tenPctButton: UIButton!
+    @IBOutlet weak var twentyPctButton: UIButton!
+    @IBOutlet weak var splitNumberLable: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func tipChanged(_ sender: Any) {
+    
+    }
+    
+    
+    @IBAction func stepperValueChanged(_ sender: Any) {
+    
+    }
+    
+    @IBAction func calculatePressed(_ sender: Any) {
+        
+    }
+    
 }
 
